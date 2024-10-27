d21285efba215f305135c1cc1376d90884dd38c2
Jezebel1990 <jezebelguedes@gmail.com>
2024-10-26 23:20:00 -0300

    feat: Add caracters and quotes in page

--- app/characters/[slug]/error.jsx
+++ app/characters/[slug]/error.jsx
@@ -11,9 +11,9 @@ export default function Error(error, reset) {
 
   return (
     <Container className="flex flex-col items-center gap-5 py-5">
-      <h1 className="text-2xl">Something Went Wrong!</h1>
+      <h1 className="text-2xl ">Something Went Wrong!</h1>
       <button
-        className="px-5 py-2 bg-blue-700 rounded-md"
+        className="px-5 py-2 bg-blue-700 rounded-md text-white"
         onClick={() => reset()}
       >
         Try again
--- next.config.js
+++ next.config.js
@@ -4,8 +4,144 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: 'images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatarfiles.alphacoders.com',
+      },
+     {
+      protocol: 'https',
+      hostname: 'facts.net',
+     },
+     {
+      protocol: 'https',
+      hostname: 'preview.redd.it',
+     },
+     {
+      protocol: 'https',
+      hostname: 'a0.anyrgb.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'steamuserimages-a.akamaihd.net',
+     },
+     {
+      protocol: 'https',
+      hostname: 'static.wikia.nocookie.net',
+     },
+     {
+      protocol: 'https',
+      hostname: 'i.imgflip.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'www.redwolf.in',
+     },
+     {
+      protocol: 'https',
+      hostname: 'i.redd.it',
+     },
+     {
+      protocol: 'https',
+      hostname: 'i.imgur.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'e1.pxfuel.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'tm.ibxk.com.br',
+     },
+     {
+      protocol: 'https',
+      hostname: 'i0.wp.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'www.1papacaio.com.br',
+     },
+     {
+      protocol: 'https',
+      hostname: 'static0.gamerantimages.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'images6.alphacoders.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'giffiles.alphacoders.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'm.media-amazon.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'images8.alphacoders.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'cdn.flickeringmyth.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'wallpapercave.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'comicbook.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'imgix.ranker.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'www.pixelstalk.net',
+     },
+     {
+      protocol: 'https',
+      hostname: '4kwallpapers.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'www.belloflostsouls.net',
+     },
+     {
+      protocol: 'https',
+      hostname: 'motionbgs.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'images5.alphacoders.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'cdn.kayiprihtim.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'static1.srcdn.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'static1.cbrimages.com',
+     },
+     {
+      protocol: 'https',
+      hostname: 'cinepop.com.br',
+     },
     ],
   },
 }
