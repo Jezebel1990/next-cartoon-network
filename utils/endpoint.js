ea71b1d07532b4510c3d36a678d2dbb22309c771
Jezebel1990 <jezebelguedes@gmail.com>
2024-10-28 23:41:20 -0300

    Update endpoint

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,3 +1,3 @@
 // export const endpoint = 'http://localhost:3000/api/'
 
-export const endpoint = 'https://next-cartoon-network.netlify.app/api/'
+export const endpoint = 'https://next-cartoon-network.vercel.app/api/'
